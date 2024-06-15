c24a2990c49720ce39444c5c631c3de3287b63c2
ting <ting04180@gmail.com>
2024-06-16 02:02:12 +0800

    0614改善延遲及新增input欄位

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 let interval;
+let h_secondsDisplay = document.getElementById("h_seconds");
 let m_secondsDisplay = document.getElementById("m_seconds");
 let secondsDisplay = document.getElementById("seconds");
+let inputMinutes = document.getElementById("inputMinutes");
+let setTimeButton = document.getElementById("setTime");
 let startTimer = document.getElementById("start");
 let stopTimer = document.getElementById("stop");
 let resetTimer = document.getElementById("reset");
 let totalSeconds = 10 * 60;
 let isRunning = false; //防止重複點擊
+
+setTimeButton.onclick = () => {
+  let minutes = parseInt(inputMinutes.value); //準換成數字計算
+  //確認是否有不合規格的value
+  if (isNaN(minutes) || minutes < 0) {
+    alert("請輸入有效的分鐘數");
+    return;
+  }
+  totalSeconds = minutes * 60;
+  inputMinutes.value = ""; // 清空輸入框內容
+  updateDisplay();
+};
 startTimer.onclick = () => {
   if (!isRunning) {
+    totalSeconds--; // 立即減少 totalSeconds
+    updateDisplay(); // 立即更新顯示
     interval = setInterval(timer, 1000);
     isRunning = true;
   }
@@ -18,7 +35,7 @@ stopTimer.onclick = () => {
 };
 resetTimer.onclick = () => {
   clearInterval(interval);
-  totalSeconds = 10 * 60;
+  totalSeconds = 0;
   updateDisplay();
   isRunning = false;
 };
@@ -27,13 +44,16 @@ let timer = () => {
     clearInterval(interval);
     alert("時間到！");
   } else {
-    updateDisplay();
     totalSeconds--;
+    updateDisplay();
   }
 };
 let updateDisplay = () => {
-  let minutes = Math.floor(totalSeconds / 60);
+  let hours = Math.floor(totalSeconds / 3600);
+  let minutes = Math.floor((totalSeconds % 3600) / 60);
   let seconds = totalSeconds % 60;
+
+  h_secondsDisplay.innerHTML = formatTime(hours);
   m_secondsDisplay.innerHTML = formatTime(minutes);
   secondsDisplay.innerHTML = formatTime(seconds);
 };
